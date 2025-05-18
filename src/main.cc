c88366a9f5f4696bc35f357da1a92361f48c64e4
Raymer8639 <diliyangemail@gmail.com>
2025-05-18 11:00:06 +0000

    change main.cc

--- src/main.cc
+++ src/main.cc
@@ -4,11 +4,26 @@
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_video.h>
 #include <array>
+#include <cstdint>
+#include <vector>
 
 int main(int argc, char** argv)
 {
+
+  std::array<SDL_Vertex, 3> sjx;
+  sjx = {
+    SDL_Vertex { { 150, 100 }, { 1.0f, 0.0f, 0.0f, 1.0f } },
+    SDL_Vertex { { 000, 300 }, { 0.0f, 1.0f, 0.0f, 1.0f } },
+    SDL_Vertex { { 300, 300 }, { 0.0f, 0.0f, 1.0f, 1.0f } }
+  };
+  uint64_t last_tickets = SDL_GetTicks();
+  float pos = 0.0f;
+  float dire = 1.0f;
+
+
   SDL_SetLogPriorities(SDL_LOG_PRIORITY_VERBOSE);
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window* window = SDL_CreateWindow("hello!", 800, 600, SDL_WINDOW_RESIZABLE);
@@ -28,13 +43,33 @@ int main(int argc, char** argv)
         }
       }
     }
-    std::array<SDL_Vertex, 3> sjx;
-    sjx = {
-      SDL_Vertex { { 150, 100 }, { 1.0f, 0.0f, 0.0f, 1.0f } },
-      SDL_Vertex { { 000, 300 }, { 0.0f, 1.0f, 0.0f, 1.0f } },
-      SDL_Vertex { { 300, 300 }, { 0.0f, 0.0f, 1.0f, 1.0f } }
-    };
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+    uint64_t current_ticks = SDL_GetTicks();
+    float delta_time = (current_ticks - last_tickets) / 1000.0f;
+    last_tickets = current_ticks;
+    pos += 200.0f * delta_time * dire;
+
+    int width = 0;
+
+    SDL_GetRenderOutputSize(renderer, &width, 0);
+
+    float max_pos = static_cast<float>(width) - (sjx[2].position.x - sjx[1].position.x);
+
+    if (pos > max_pos)
+      dire = -1.0f;
+    else if (pos < 0.0f)
+    {
+      pos = 0.0f;
+      dire = 1.0f;
+    }
+
+    std::vector<SDL_Vertex> vertices;
+    for (const auto& vertex : sjx)
+    {
+      vertices.push_back(vertex);
+      vertices.back().position.x += pos;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
     SDL_RenderGeometry(renderer, 0, sjx.data(), sjx.size(), 0, 0);
     SDL_RenderPresent(renderer);
