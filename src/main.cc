663e94bf1d88313b3bb4ebf85b42967d4285fa5d
Raymer8639 <diliyangemail@gmail.com>
2025-05-18 11:14:03 +0000

    change main.cc

--- src/main.cc
+++ src/main.cc
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window* window = SDL_CreateWindow("hello!", 800, 600, SDL_WINDOW_RESIZABLE);
   SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, "vulkan");
 
   bool is_running = true;
   SDL_Event event {};
